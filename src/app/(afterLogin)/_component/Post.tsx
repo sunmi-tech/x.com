534ac1eed91c24fbb3eecd87beb6b6c5b07bde87
sunmi An <aseonmi220@gmail.com>
2025-07-13 21:09:56 +0900

    feat: photo page 완성

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -1,32 +1,46 @@
-import style from './post.module.css';
+import style from "./post.module.css";
 import Link from "next/link";
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import 'dayjs/locale/ko';
-import ActionButtons from './ActionButtons';
-import PostArticle from './PostArticle';
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko";
+import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import PostArticle from "@/app/(afterLogin)/_component/PostArticle";
+import { faker } from "@faker-js/faker";
+import PostImages from "./PostImages";
 
-dayjs.locale('ko');
-dayjs.extend(relativeTime)
+dayjs.locale("ko");
+dayjs.extend(relativeTime);
 
-export default function Post() {
+type Props = {
+  noImage?: boolean;
+};
+export default function Post({ noImage }: Props) {
   const target = {
+    postId: 1,
     User: {
-      postId: 1,
-      id: 'elonmusk',
-      nickname: 'Elon Musk',
-      image: '/logo.svg',
+      id: "elonmusk",
+      nickname: "Elon Musk",
+      image: "/yRsRRjGO.jpg",
     },
-    content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
+    content: "클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ",
     createdAt: new Date(),
-    Images: [],
+    Images: [] as { imageId: number; link: string }[],
+  };
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push(
+      { imageId: 1, link: faker.image.url() },
+      { imageId: 2, link: faker.image.url() },
+      { imageId: 3, link: faker.image.url() },
+      { imageId: 4, link: faker.image.url() }
+    );
   }
+
   return (
-    <PostArticle post={target} className={style.post}>
+    <PostArticle post={target}>
       <div className={style.postWrapper}>
         <div className={style.postUserSection}>
           <Link href={`/${target.User.id}`} className={style.postUserImage}>
-            <img src={target.User.image} alt={target.User.nickname}/>
+            <img src={target.User.image} alt={target.User.nickname} />
             <div className={style.postShade} />
           </Link>
         </div>
@@ -36,19 +50,19 @@ export default function Post() {
               <span className={style.postUserName}>{target.User.nickname}</span>
               &nbsp;
               <span className={style.postUserId}>@{target.User.id}</span>
-              &nbsp;
-              ·
-              &nbsp;
+              &nbsp; · &nbsp;
             </Link>
-            <span className={style.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
+            <span className={style.postDate}>
+              {dayjs(target.createdAt).fromNow(true)}
+            </span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}>
-
+          <div>
+            <PostImages post={target} />
           </div>
           <ActionButtons />
         </div>
       </div>
     </PostArticle>
-  )
+  );
 }
